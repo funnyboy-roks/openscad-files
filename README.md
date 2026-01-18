5066f7d7cf21f3b77cdf09f3924ae16159f62e2d
funnyboy-roks <fbr@fbr.dev>
2026-01-18 02:40:42 -0600

    readme stuff

--- README.md
+++ README.md
@@ -18,3 +18,20 @@ openscad --viewall <file.scad>
 ```sh
 openscad <file.scad> -o <file.stl>
 ```
+
+## Contents
+
+> [!NOTE]
+>
+> This sections is auto generated (ish) and may not be 100% perfect.
+
+# `lamy-t10.scad`
+
+Clip for holding five LAMY T10 ink cartridges (https://www.lamy.com/en-us/p/lamy-t-10-ink/50727869088078)
+
+![Rendered Image](./img/lamy-t10.png)
+
+# `wire-rack-surface.scad`
+
+![Rendered Image](./img/wire-rack-surface.png)
+
---
+++ gen-readme.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+out=README.md
+
+cat > README.md <<EOF
+# OpenSCAD Files
+
+This is a collection of random things that I've modelled in [OpenSCAD]
+for personal use.
+
+[OpenSCAD]: https://openscad.org/
+
+## Development
+
+OpenSCAD may be opened in a window with automatic updates using
+
+\`\`\`sh
+openscad --viewall <file.scad>
+\`\`\`
+
+## Rendering
+
+\`\`\`sh
+openscad <file.scad> -o <file.stl>
+\`\`\`
+
+## Contents
+
+> [!NOTE]
+>
+> This sections is auto generated (ish) and may not be 100% perfect.
+
+EOF
+
+mkdir -p img
+
+for file in $(git ls-files | grep '\.scad$'); do
+    echo "Generating from $file"
+    header=$(head -n1 "$file")
+
+    echo -e "# \`$file\`\n" >> $out
+
+    if [[ $header == '// '* ]]; then
+        header=${header:3}
+        echo "header: $header"
+        echo -e "$header\n" >> $out
+    fi
+
+    out_png="img/${file%.scad}.png"
+    echo "Generating $out_png"
+    openscad --preview png "$file" --imgsize=1024,1024 -o "$out_png" --colorscheme DeepOcean
+
+    echo -e "![Rendered Image](./$out_png)\n" >> $out
+done
